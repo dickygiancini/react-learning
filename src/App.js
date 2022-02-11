2f2cfe710f0cf4f518fc24567b635d738dad37f9
dickygiancini <dicky.giancini.123@gmail.com>
2022-02-11 17:27:29 +0700

    Title, Body paragraph and button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 function App() {
   return (
-    <div>Hello World!</div>
+    <div>
+      <h1>Todos</h1>
+      <div>
+        <h2>TITLE</h2>
+        <div>
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
