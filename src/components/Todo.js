d084e31a5c23a80d2d4c3798a9bb5b90a7e11efc
dickygiancini <dicky.giancini.123@gmail.com>
2022-02-15 19:25:15 +0700

    Confirmation modal

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,39 +1,43 @@
 import { useState } from "react";
-import Modal from "./Modal";
-import Backdrop from "./Backdrop";
-import { Card, Button, Col } from "react-bootstrap";
+// import Modal from "./Modal";
+// import Backdrop from "./Backdrop";
+import { Card, Button, Col, Modal } from "react-bootstrap";
 
 function Todo(props) {
-  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [show, setShow] = useState(false);
 
-  function openModalHandler() {
-    setModalIsOpen(true);
-  }
-
-  function closeModalHandler() {
-    setModalIsOpen(false);
-  }
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
-      <Col md={6}>
-        <Card>
-          <Card.Body>
-            <h2>{props.text}</h2>
-            <div className="actions">
-              <Button variant="danger" onClick={openModalHandler}>
-                Delete
+    <Col md={6}>
+      <Card>
+        <Card.Body>
+          <h2>{props.text}</h2>
+          <div className="actions">
+            <Button variant="danger" onClick={handleShow}>
+              Delete
+            </Button>
+          </div>
+
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Confirmation</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Are you sure?</Modal.Body>
+
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
               </Button>
-            </div>
-            {modalIsOpen && (
-              <Modal
-                onClick={closeModalHandler}
-                onConfirm={closeModalHandler}
-              />
-            )}
-            {modalIsOpen && <Backdrop onClick={closeModalHandler} />}
-          </Card.Body>
-        </Card>
-      </Col>
+              <Button variant="primary" onClick={handleClose}>
+                Confirm
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </Card.Body>
+      </Card>
+    </Col>
   );
 }
 
