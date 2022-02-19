c8db9eb37f4bd0510271a315c5992b5d0e5784e8
dickygiancini <dicky.giancini.123@gmail.com>
2022-02-19 15:14:57 +0700

    Navigation CSS Module

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,16 +1,26 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
-    return <header>
-        <div>React Meetups</div>
-        <nav>
-            <ul>
-                <li><Link to='/'>All Meetups</Link></li>
-                <li><Link to='/new-meetup'>New Meetups</Link></li>
-                <li><Link to='/favourite'>Favourite</Link></li>
-            </ul>
-        </nav>
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/new-meetup">New Meetups</Link>
+          </li>
+          <li>
+            <Link to="/favourite">Favourite</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
+  );
 }
 
 export default MainNavigation;
