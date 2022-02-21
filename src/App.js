e647f3a4399165a0cd39b87bba06c0c827a74292
dickygiancini <dicky.giancini.123@gmail.com>
2022-02-21 17:38:34 +0700

    Children props

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favourite";
 import NewMeetupPage from "./pages/NewMeetup";
 import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path='/' element={<AllMeetupsPage />}></Route>
         <Route path='/new-meetup' element={<NewMeetupPage />}></Route>
         <Route path='/favourite' element={<FavoritesPage />}></Route>
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
